e5f035d2c590015779d98e2b0c281e7b83dcf52b
DevExpressExampleBot <fakeEmail>
2023-02-15 12:48:35 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,23 +1,24 @@
-﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core
 Imports System.Windows
 
 Namespace DisableMasterDetailWizard
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub UseWizardCustomizationService(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Call New DXTabbedWindow() With {.Content = New UseWizardCustomization()}.ShowDialog()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub UseServicesRegistry(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Call New DXTabbedWindow() With {.Content = New UseServicesRegistry()}.ShowDialog()
-		End Sub
-	End Class
+        Private Sub UseWizardCustomizationService(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Call New DXTabbedWindow() With {.Content = New UseWizardCustomization()}.ShowDialog()
+        End Sub
+
+        Private Sub UseServicesRegistry(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Call New DXTabbedWindow() With {.Content = New UseServicesRegistry()}.ShowDialog()
+        End Sub
+    End Class
 End Namespace
